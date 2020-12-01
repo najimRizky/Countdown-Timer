856b656fdad204a4260043a511c563d0204627fe
najimRizky <najim.rizky@student.umn.ac.id>
2020-12-01 21:19:22 +0700

    Sisa Pause

---
+++ src/components/Timer.vue
@@ -0,0 +1,209 @@
+<template>
+    <div class="timer1" >
+        <v-container>
+            <v-row text="center">
+                <v-col align="center">
+                <h2>⏳ Countdown Timer ⏳</h2>
+                </v-col>
+            </v-row>
+            <v-row id="timer1">
+                <v-spacer></v-spacer>
+                <v-col cols="10" align="center">
+                    <v-row>
+                        <v-col cols="3" style="padding: 0px"><img src="@/assets/segitigaUp.png" @click="addJam()"></v-col>
+                        <v-spacer></v-spacer>
+                        <v-col cols="3" style="padding: 0px"><img src="@/assets/segitigaUp.png" @click="addMenit()"></v-col>
+                        <v-spacer></v-spacer>
+                        <v-col cols="3" style="padding: 0px"><img src="@/assets/segitigaUp.png" @click="addDetik()"></v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="3"><div>{{jam || '00'}}</div></v-col>
+                        <v-col cols="1">:</v-col>
+                        <v-spacer></v-spacer>
+                        <v-col cols="3"><div>{{menit || '00'}}</div></v-col>
+                        <v-col cols="1">:</v-col>
+                        <v-spacer></v-spacer>
+                        <v-col cols="3"><div>{{detik || '00'}}</div></v-col>
+                    </v-row>
+                    <v-row style="font-weight: normal; font-size: 12px;">
+                        <v-col cols="3" style="padding: 0px"><span>Hours</span></v-col>
+                        <v-spacer></v-spacer>
+                        <v-col cols="3" style="padding: 0px"><span>Minutes</span></v-col>
+                        <v-spacer></v-spacer>
+                        <v-col cols="3" style="padding: 0px"><span>Seconds</span></v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="3"><img src="@/assets/segitigaDown.png" @click="decJam()"></v-col>
+                        <v-spacer></v-spacer>
+                        <v-col cols="3"><img src="@/assets/segitigaDown.png" @click="decMenit()"></v-col>
+                        <v-spacer></v-spacer>
+                        <v-col cols="3"><img src="@/assets/segitigaDown.png" @click="decDetik()"></v-col>
+                    </v-row>
+                    <v-row v-if="!timerStats">
+                        <v-col cols="12">
+                            <v-btn  color="green" v-on:click="startTimer()">Start</v-btn>
+                        </v-col>
+                    </v-row>
+                    <v-row v-if="timerStats">
+                        <v-col cols="12">
+                            <v-btn color="green" style="margin:0px 5px"  v-if="pauseStats">Start</v-btn>
+                            <v-btn color="yellow" style="margin:0px 5px" @click="pauseTimer()" v-if="!pauseStats">Pause</v-btn>
+                            <v-btn color="red" style="margin:0px 5px" @click="resetTimer()">Reset</v-btn>
+                        </v-col>
+                    </v-row>
+                </v-col>
+                <v-spacer></v-spacer>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            jam: 0, menit: 0, detik: 0,
+            timerStats: false,
+            pauseStats: false,
+            tmpJam: 0, tmpMenit: 0, tmpDetik: 0,
+            tmp2Jam: 0, tmp2Menit: 0, tmp2Detik: 0,
+            i:0,
+            currThn:0,
+            currBln:0,
+            currTgl:0,
+            currJam:0,
+            currMenit:0,
+            currDetik:0,
+        }),
+        computed: {
+            _detiks: () => 1000,
+            _menits(){
+                return this._detiks * 60;
+            },
+            _jams(){
+                return this._menits * 60;
+            }
+        },
+        methods: {
+            addJam(){
+                this.tmpJam += 1;
+                this.jam = this.formatNum(this.tmpJam)
+            },
+            addMenit(){
+                if(this.tmpMenit < 59){
+                    this.tmpMenit += 1;
+                }else{
+                    this.tmpJam += 1
+                    this.tmpMenit = 0;
+                }
+                this.menit = this.formatNum(this.tmpMenit)
+                this.jam = this.formatNum(this.tmpJam)    
+                this.detik = this.formatNum(this.tmpDetik)    
+            },
+            addDetik(){
+                if(this.tmpDetik < 59){
+                    this.tmpDetik += 1;
+                }else{
+                    this.tmpMenit += 1
+                    this.tmpDetik = 0;
+                }
+                this.menit = this.formatNum(this.tmpMenit)
+                this.jam = this.formatNum(this.tmpJam)    
+                this.detik = this.formatNum(this.tmpDetik)  
+            },
+            decJam(){
+                if(this.tmpJam >0){
+                    this.tmpJam -= 1;
+                }
+                this.jam = this.formatNum(this.tmpJam)    
+            },
+            decMenit(){
+                if(this.tmpMenit >0){
+                    this.tmpMenit -= 1;
+                }
+                this.menit = this.formatNum(this.tmpMenit)
+            },
+            decDetik(){
+                if(this.tmpDetik >0){
+                    this.tmpDetik -= 1;
+                }
+                this.detik = this.formatNum(this.tmpDetik)  
+            },
+            startTimer(){
+                this.showRemaining()
+            },
+            formatNum: num => (num < 10 ? '0' + num : num),
+            showRemaining(){
+                if(this.tmpJam > 0 || this.tmpMenit > 0 || this.tmpDetik > 0){
+                    this.timerStats = true;
+                    this.pauseStats = false;
+                    const timerz = setInterval(() => {
+                        const now = new Date();
+                        if(this.i==0){
+                            this.currThn = now.getFullYear();
+                            this.currBln = now.getMonth();
+                            this.currTgl = now.getDate();
+                            this.currJam = now.getHours() + this.tmpJam;
+                            this.currMenit = now.getMinutes() + this.tmpMenit;
+                            this.currDetik = now.getSeconds()+ this.tmpDetik;     
+                            this.i += 1;
+                        }
+                        const end = new Date(this.currThn,this.currBln,this.currTgl,this.currJam, this.currMenit, this.currDetik);
+                        const distance = end.getTime() - now.getTime();
+                        if(distance < 0){
+                            clearInterval(timerz);
+                            this.tmpJam = 0;
+                            this.tmpMenit = 0;
+                            this.tmpDetik = 0;
+                            this.i = 0;
+                            this.timerStats = false;
+                            return;
+                        }
+                        const jams = Math.floor(distance / this._jams);
+                        const menits = Math.floor((distance % this._jams) / this._menits);
+                        const detiks = Math.floor((distance % this._menits) / this._detiks);
+                        this.jam = this.formatNum(jams);
+                        this.menit = this.formatNum(menits);
+                        this.detik = this.formatNum(detiks);
+                    }, 1000);
+                }
+            },
+            resetTimer(){
+                clearInterval(this.timerz);
+                this.tmpJam = 0; this.jam = this.formatNum(this.tmpJam);
+                this.tmpMenit = 0; this.menit = this.formatNum(this.tmpMenit);
+                this.tmpDetik = 0; this.detik = this.formatNum(this.tmpDetik);
+                this.i = 0;
+                this.timerStats = false;
+                return;
+            },
+            pauseTimer(){
+                clearInterval(this.timerz);
+                this.pauseStats = true;
+                /*this.tmp2Jam = ;
+                this.tmp2Menit = ;
+                this.tmp2Detik = ;*/
+                this.tmpJam = 0;
+                this.tmpMenit = 0;
+                this.tmpDetik = 0;
+                return;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    img{
+        width: 30px;
+        cursor: pointer;
+    }
+    #timer1{
+        font-size: 30px;
+        font-weight: bold;
+    }
+    .timer1{
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    height: 100%;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Timer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Timer from '@/components/Timer.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components:{
+    Timer
+  },
 }
 </script>
+<style scoped>
+  .home{
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    height: 100%;
+  }
+</style>
